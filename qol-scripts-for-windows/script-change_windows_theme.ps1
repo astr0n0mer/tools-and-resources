ca881b49681f022948f53846fc383fd6c4558a2e
Imran Khan (MAQ LLC) <v-khanimr@microsoft.com>
2022-12-17 22:27:17 +0530

    Add PS script to change Windows theme

---
+++ qol-scripts-for-windows/script-change_windows_theme.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [string]$theme_name = "dark"
+)
+
+$command = "C:\Windows\Resources\Themes\" + $theme_name + ".theme"
+Invoke-Expression $command
+# Invoke-Expression $command opens the settings app by default
+# Script sleeps for 2 seconds to allow the settings app to open and then "SystemSettings" process is stopped
+Start-Sleep -Seconds 2
+Stop-Process -Name SystemSettings
