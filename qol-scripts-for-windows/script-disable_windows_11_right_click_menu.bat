3dab40475f267375b50f9d8cbe83e7fae11ecc23
Imran Khan <imran2000786@gmail.com>
2023-04-14 17:53:36 +0530

    Add another source article to disable Windows 11 context menu

--- qol-scripts-for-windows/script-disable_windows_11_right_click_menu.bat
+++ qol-scripts-for-windows/script-disable_windows_11_right_click_menu.bat
@@ -1,5 +1,6 @@
 @echo off
-rem Source: https://techdows.com/2021/10/disable-or-enable-windows-11-new-context-menu.html
+rem Source #1: https://techdows.com/2021/10/disable-or-enable-windows-11-new-context-menu.html
+rem Source #2: https://www.anoopcnair.com/windows-11-context-menu-how-to-disable-enable#:~:text=want%20to%20disable
 
 @reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 pause
